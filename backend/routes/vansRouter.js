29ed4ea0ec3f42b2cec6225110108ad58e088e00
zahirul <zahirulopel@gmail.com>
2024-09-28 15:35:11 +0600

    use api/van/uploads uri to upload photos

--- backend/routes/vansRouter.js
+++ backend/routes/vansRouter.js
@@ -1,5 +1,22 @@
 const express = require("express");
 const vanRouter = express.Router();
+const crypto = require("crypto");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    const nameOfFile = `${crypto
+      .randomBytes(4)
+      .toString("hex")}${file.originalname.split(" ").join("")}`;
+    console.log(nameOfFile);
+    cb(null, nameOfFile);
+  },
+});
+var upload = multer({ storage: storage });
+vanRouter.use("/uploads", express.static("uploads"));
 
 vanRouter.get("/van", (req, res) => {
   res.status(200).send("<h1>Van route</h1>");
@@ -8,6 +25,10 @@ vanRouter.get("/van", (req, res) => {
 vanRouter.get("/van/:id", (req, res) => {
   const { id } = req.params;
   res.status(200).send(`<h1>Van id ${id}</h1>`);
+});
+
+vanRouter.post("/van/uploads", upload.single("bus"), (req, res) => {
+  res.json(req.file);
 });
 
 module.exports = vanRouter;
