a0d9e95b322646ba1be2b704a676f0f45c86ae29
zahirul <zahirulopel@gmail.com>
2024-10-09 16:37:16 +0600

    page:van page style added

--- frontEnd/src/Pages/Vans/VansPage.jsx
+++ frontEnd/src/Pages/Vans/VansPage.jsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 import VanService from "../../services/van";
 import VanInfo from "../../Components/VanInfo";
+import styles from "../../styles/pages/Vans/vanspage.module.css";
 
 const VansPage = ({}) => {
   const [vansData, setVansData] = useState([]);
@@ -11,24 +14,40 @@ const VansPage = ({}) => {
         const vansInformation = await VanService.getAllVansData();
         console.log("van info", vansInformation);
         setVansData(vansInformation);
-      } catch (error) {}
+      } catch (error) {
+        console.error(error.message);
+      }
     };
     getVanData();
   }, []);
 
   return (
-    <div>
-      {vansData.length > 0
-        ? vansData.map((van) => (
-            <VanInfo
-              key={van.id}
-              vanImgSrc={van.path}
-              name={van.name}
-              price={van.rent}
-              type={van.type}
-            />
-          ))
-        : null}
+    <div className={styles.vansPageContainer}>
+      <header>
+        <Navbar
+          pagesList={[
+            { name: "About", id: crypto.randomUUID() },
+            { name: "Vans", id: crypto.randomUUID() },
+          ]}
+        />
+      </header>
+      <main>
+        {vansData.length > 0
+          ? vansData.map((van) => (
+              <VanInfo
+                key={van.id}
+                vanImgSrc={van.path}
+                name={van.name}
+                price={van.rent}
+                type={van.type}
+              />
+            ))
+          : null}
+      </main>
+
+      <footer>
+        <Footer text="VANLIFE" />
+      </footer>
     </div>
   );
 };
