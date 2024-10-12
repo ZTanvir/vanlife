e3141324ff9fc3d6b63452e535f64fd00a65dd2f
zahirul <zahirulopel@gmail.com>
2024-10-12 16:07:26 +0600

    page:add navbar and footer component to van page

--- frontEnd/src/Pages/Van/VanPage.jsx
+++ frontEnd/src/Pages/Van/VanPage.jsx
@@ -1,6 +1,8 @@
 import VanInfo from "../../Components/VanInfo";
 import Button from "../../Components/Button";
 import VanService from "../../services/van";
+import Navbar from "../../Components/Navbar";
+import Footer from "../../Components/Footer";
 import styles from "../../styles/pages/Van/vanpage.module.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
@@ -24,24 +26,38 @@ const VanPage = ({}) => {
   }, []);
 
   return (
-    <div className={styles.vanInformationContainer}>
-      {vanData.length > 0 &&
-        (vanData[0].message ? (
-          <h1>Van not found</h1>
-        ) : (
-          <VanInfo
-            vanImgSrc={vanData[0].path}
-            name={vanData[0].name}
-            price={vanData[0].rent}
-            type={vanData[0].type}
-          />
-        ))}
-      <Button
-        text="Rent this van"
-        bgColor="#FF8C38"
-        btnColor="white"
-        hoverColor="#ffac70"
-      />
+    <div className={styles.vanPage}>
+      <header>
+        <Navbar
+          pagesList={[
+            { name: "About", id: crypto.randomUUID() },
+            { name: "Vans", id: crypto.randomUUID() },
+          ]}
+        />
+      </header>
+      <main>
+        {vanData.length > 0 &&
+          (vanData[0].message ? (
+            <h1>Van not found</h1>
+          ) : (
+            <VanInfo
+              vanImgSrc={vanData[0].path}
+              name={vanData[0].name}
+              price={vanData[0].rent}
+              type={vanData[0].type}
+            />
+          ))}
+        <Button
+          text="Rent this van"
+          bgColor="#FF8C38"
+          btnColor="white"
+          hoverColor="#ffac70"
+        />
+      </main>
+
+      <footer>
+        <Footer text="VANLIFE" />
+      </footer>
     </div>
   );
 };
